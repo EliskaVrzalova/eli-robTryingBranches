c84337d9418ccac3f6667a635385395d18bedfef
(Student  C00302210) Robert Mc Gregor <C00302210@setu.ie>
2023-11-09 18:18:02 +0000

    Have added 'even' code.

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,8 +1,20 @@
 //eliska odd
+//rob even
 
 #include <iostream>
+
 int main()
 {
+	int inputNumber = 0;
+
+	std::cout << "WHAT IS YOUR NUMBER, ORGANIC LIFE FORM?!" << std::endl;
+	std::cin >> inputNumber;
+
+	if (inputNumber % 2 == 0) 
+	{
+		std::cout << "OH, excuse me, your number is even, that's quite reasonable." << std::endl;
+	}
+
 	system("pause");
 	return 1;
  }
