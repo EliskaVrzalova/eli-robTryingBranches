5b462b828e67224674661b4f36bda0f5a766af6e
(Student  C00301768) Eliska Vrzalova <C00301768@setu.ie>
2023-11-09 18:23:40 +0000

    odds are here again

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -3,6 +3,15 @@
 #include <iostream>
 int main()
 {
+	int number = 6;
+	//input
+	std::cout << "HEY YOU! NUMBER:" << std::endl;
+	std::cin >> number;
+
+	if (number % 2 == 1) {
+		std::cout << "GUESS WHATTTT, ITS ODD" << std::endl;
+	}
+
 	system("pause");
 	return 1;
  }
